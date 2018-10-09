8e9aeef5634a85683fd09fc457f122f72243a0ae
NickGH329 <nicgh329@icloud.com>
2018-10-09 13:01:17 +1100

    Add files via upload

---
+++ spark/src/Centroid.java
@@ -0,0 +1,28 @@
+package spark;
+
+public class Centroid{
+	private double x, y, z;
+	public Centroid(double x, double y, double z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public double getX(){
+		return x;
+	}
+	public double getY(){
+		return y;
+	}
+	public double getZ(){
+		return z;
+	}
+	public void setX(double x){
+		this.x = x;
+	}
+	public void setY(double y){
+		this.y = y;
+	}
+	public void setZ(double z){
+		this.z = z;
+	}
+}
